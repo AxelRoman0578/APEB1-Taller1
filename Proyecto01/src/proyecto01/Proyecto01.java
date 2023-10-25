c6ec297147f30d5593177ce7b07b82ff60c34272
Axel Stewart Roman Torres <asroman3@utpl.edu.ec>
2023-10-24 21:49:52 -0500

    segundo commit

--- Proyecto01/src/proyecto01/Proyecto01.java
+++ Proyecto01/src/proyecto01/Proyecto01.java
@@ -4,7 +4,7 @@ package proyecto01;
 public class Proyecto01 {
 
     public static void main(String[] args) {
-        System.out.println( "Ejemplo basico de Java Versionado a traves de Git Manejado desde Github");
+        System.out.println( "Ejemplo basico de Java Manejado desde github Versionado a traves de Git Creado por AxelRoman0578");
     }
     
 }
